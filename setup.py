6a0d8c453d431929104aceeacc78d74f953bce1c
Shivam-21-11 <shivamsingh2111@gmail.com>
2022-10-06 21:16:46 +0530

    init

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="cv-playground",
-    version="0.0.1",
+    version="0.0.2",
     description="Various Deep Learning Models (tensorflow)",
     long_description=readme(),
     long_description_content_type="text/markdown",
